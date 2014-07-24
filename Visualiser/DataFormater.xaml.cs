2aef8d20f8a8bbea01b5dd1a4229ff30905e851d
Anthz <anthquinn92@gmail.com>
2014-07-24 15:59:48 +0100

    Data formatter
    Added new window to specify the data format to be parsed into OpenTK to
    be rendered.
    Resolution/window size independent changes added to the whole program.

---
+++ Visualiser/DataFormater.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Visualiser
+{
+    /// <summary>
+    /// Interaction logic for DataFormater.xaml
+    /// </summary>
+    public partial class DataFormater : Window
+    {
+        public DataFormater()
+        {
+            InitializeComponent();
+        }
+
+        private void dataFormatCancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+
+        private void dataFormatOKButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+    }
+}
--- Visualiser/MainWindow.xaml.cs
+++ Visualiser/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ namespace Visualiser
     public partial class MainWindow : Window
     {
         OpenTKFull openTKFull;
+        List<string> frameData;
         
         public MainWindow()
         {
@@ -30,6 +31,7 @@ namespace Visualiser
         {
             OpenTKControl.Initialise();
             openTKHost.Child = OpenTKControl.openTKWindow;
+            frameData = new List<string>();
         }
 
         private void fullScreenButton_Click(object sender, RoutedEventArgs e)
@@ -55,38 +57,85 @@ namespace Visualiser
 
         private void newButton_Click(object sender, RoutedEventArgs e)
         {
+            Reset();
+        }
 
+        private void Reset()
+        {
+            frameData.Clear();
+            dataTextBox.Clear();
+            leapCheckBox.IsChecked = false;
+            riftCheckBox.IsChecked = false;
+            animatedCheckBox.IsChecked = false;
+            modelComboBox.SelectedIndex = 0;
+            dataTextBox.Visibility = Visibility.Visible;
+            animationGrid.Visibility = Visibility.Hidden;
+            loadDataButton.Content = "Load Data";
         }
 
         private void dataFormatButton_Click(object sender, RoutedEventArgs e)
         {
+            DataFormater formatWindow = new DataFormater();
+            Nullable<bool> result = formatWindow.ShowDialog();
 
+            if(result == true)
+            {
+                //format set
+            }
+            else
+            {
+                //format cancelled
+            }
         }
 
         private void loadDataButton_Click(object sender, RoutedEventArgs e)
         {
+            if(loadDataButton.Content.ToString() == "Load Data")
+            {
+                FileLoader(false);
+            }
+            else
+            {
+                FileLoader(true);
+            }
+
+            frameCountTextBlock.Text = frameData.Count + " Frames Loaded";
+        }
+
+        private void FileLoader(bool multi)
+        {
             Microsoft.Win32.OpenFileDialog filePicker = new Microsoft.Win32.OpenFileDialog();
+
+            if(multi)
+                filePicker.Multiselect = true;
 
             Nullable<bool> result = filePicker.ShowDialog();
 
             if(result == true)
             {
-                string fileName = filePicker.FileName;
-                string[] lines = System.IO.File.ReadAllLines(fileName);
-                string combinedText = "";
-                for(int i = 0; i < lines.Length; i++)
+                string[] fileNames = filePicker.FileNames;
+                for(int i = 0; i < fileNames.Length; i++)
                 {
-                    if(i != lines.Length - 1)
+                    string[] lines = System.IO.File.ReadAllLines(fileNames[i]);
+                    string combinedText = "";
+                    for (int j = 0; j < lines.Length; j++)
                     {
-                        combinedText += lines[i] + "\r\n";
+                        if (j != lines.Length - 1)
+                        {
+                            combinedText += lines[j] + "\r\n";
+                        }
+                        else
+                        {
+                            combinedText += lines[j];   //dont add carriage return on last entry
+                        }
                     }
-                    else
-                    {
-                        combinedText += lines[i];   //dont add carriage return on last entry
-                    }
-                }
 
-                dataTextBox.Text = combinedText;
+                    frameData.Add(combinedText);
+
+                    if(frameData.Count == 1)
+                        dataTextBox.Text = combinedText;
+                }
+                
             }
         }
 
@@ -109,6 +158,39 @@ namespace Visualiser
         private void modelComboBox_Loaded(object sender, RoutedEventArgs e)
         {
             modelComboBox.SelectedIndex = 0;
+        }
+
+        private void animatedCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            dataTextBox.Visibility = Visibility.Hidden;
+            animationGrid.Visibility = Visibility.Visible;
+            loadDataButton.Content = "Load Frames";
+            frameCountTextBlock.Text = frameData.Count + " Frames Loaded";
+        }
+
+        private void animatedCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if (frameData.Count > 1)
+            {
+                string messageText = "Multiple frames are currently loaded. To return to single data mode, please start a new session.\r\n" +
+                                        "Do you wish to start a new session now?";
+                string messageTitle = "Start New Session?";
+                MessageBoxButton button = MessageBoxButton.YesNo;
+                MessageBoxImage icon = MessageBoxImage.Question;
+
+                MessageBoxResult result = MessageBox.Show(messageText, messageTitle, button, icon);
+
+                if (result == MessageBoxResult.Yes)
+                {
+                    Reset();
+                }
+            }
+            else
+            {
+                dataTextBox.Visibility = Visibility.Visible;
+                animationGrid.Visibility = Visibility.Hidden;
+                loadDataButton.Content = "Load Data";
+            }
         }
     }
 }
