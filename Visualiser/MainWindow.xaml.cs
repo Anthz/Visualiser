adc2d955ba98aa93f488efb81e10844a609d7a45
Anthz <anthquinn92@gmail.com>
2014-07-24 10:37:36 +0100

    Initial project added
    Includes embedded OpenTK window/full screen, settings and data input.

---
+++ Visualiser/MainWindow.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace Visualiser
+{
+    public partial class MainWindow : Window
+    {
+        OpenTKFull openTKFull;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            OpenTKControl.Initialise();
+            openTKHost.Child = OpenTKControl.openTKWindow;
+        }
+
+        private void fullScreenButton_Click(object sender, RoutedEventArgs e)
+        {
+            openTKFull = new OpenTKFull();
+            Nullable<bool> result = openTKFull.ShowDialog();
+
+            if(result == false)
+            {
+                openTKHost.Child = OpenTKControl.openTKWindow;
+            }
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void openButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void newButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void dataFormatButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void loadDataButton_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog filePicker = new Microsoft.Win32.OpenFileDialog();
+
+            Nullable<bool> result = filePicker.ShowDialog();
+
+            if(result == true)
+            {
+                string fileName = filePicker.FileName;
+                string[] lines = System.IO.File.ReadAllLines(fileName);
+                string combinedText = "";
+                for(int i = 0; i < lines.Length; i++)
+                {
+                    if(i != lines.Length - 1)
+                    {
+                        combinedText += lines[i] + "\r\n";
+                    }
+                    else
+                    {
+                        combinedText += lines[i];   //dont add carriage return on last entry
+                    }
+                }
+
+                dataTextBox.Text = combinedText;
+            }
+        }
+
+        private void riftCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            string messageText = "To enable Oculus Rift stereoscopy and head tracking, full screen is required.\r\n" +
+                                    "Do you wish to enter full screen mode now?";
+            string messageTitle = "Switch To Full Screen?";
+            MessageBoxButton button = MessageBoxButton.YesNo;
+            MessageBoxImage icon = MessageBoxImage.Question;
+
+            MessageBoxResult result = MessageBox.Show(messageText, messageTitle, button, icon);
+
+            if(result == MessageBoxResult.Yes)
+            {
+                fullScreenButton_Click(null, null);
+            }
+        }
+
+        private void modelComboBox_Loaded(object sender, RoutedEventArgs e)
+        {
+            modelComboBox.SelectedIndex = 0;
+        }
+    }
+}
---
+++ Visualiser/OpenTKControl.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace Visualiser
+{
+    public static class OpenTKControl
+    {
+        public static GLControl openTKWindow;
+
+        public static void Initialise()
+        {
+            openTKWindow = new GLControl();
+
+            openTKWindow.Load += OpenTKWindow_Load;
+            openTKWindow.Paint += OpenTKWindow_Paint;
+        }
+
+        public static void OpenTKWindow_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+
+            // Draw a little yellow triangle
+            GL.Color3(System.Drawing.Color.Yellow);
+            GL.Begin(BeginMode.Triangles);
+            GL.Vertex2(200, 50);
+            GL.Vertex2(200, 200);
+            GL.Vertex2(100, 50);
+            GL.End();
+
+            openTKWindow.SwapBuffers();
+        }
+
+        public static void OpenTKWindow_Load(object sender, EventArgs e)
+        {
+            GL.ClearColor(System.Drawing.Color.CornflowerBlue);
+
+            int w = openTKWindow.Width;
+            int h = openTKWindow.Height;
+
+            // Set up initial modes
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.Ortho(0, w, 0, h, -1, 1);
+            GL.Viewport(0, 0, w, h);
+        }
+    }
+}
