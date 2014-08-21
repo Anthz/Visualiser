5fa2b3fc00917e9a32add26c3bd7ed2b7b545049
Anthz <anthquinn92@gmail.com>
2014-08-21 15:52:19 +0100

    Framebuffer fix
    Framebuffer fixed and texture is now being rendered.
    Texture needs to be split in y axis for stereoscopic view
    (viewport/scissor)

--- Visualiser/OpenTKControl.cs
+++ Visualiser/OpenTKControl.cs
@@ -32,16 +32,18 @@ namespace Visualiser
 
             textureID = GL.GenTexture();
             GL.BindTexture(TextureTarget.Texture2D, textureID);
-            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgb, width, height, 0, PixelFormat.Rgb, PixelType.UnsignedByte, new IntPtr(0));
-            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);  //try .Nearest
-            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);  //ditto
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgb, width, height, 0, PixelFormat.Rgb, PixelType.UnsignedByte, IntPtr.Zero);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Nearest);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Nearest);
 
             renderBufferID = GL.GenRenderbuffer();
             GL.BindRenderbuffer(RenderbufferTarget.Renderbuffer, renderBufferID);
             GL.RenderbufferStorage(RenderbufferTarget.Renderbuffer, RenderbufferStorage.DepthComponent, width, height);
+
+            GL.FramebufferTexture2D(FramebufferTarget.Framebuffer, FramebufferAttachment.ColorAttachment0, TextureTarget.Texture2D, textureID, 0);
             GL.FramebufferRenderbuffer(FramebufferTarget.Framebuffer, FramebufferAttachment.DepthAttachment, RenderbufferTarget.Renderbuffer, renderBufferID);
 
-            GL.FramebufferTexture(FramebufferTarget.FramebufferExt, FramebufferAttachment.ColorAttachment0, textureID, 0);
+            Console.WriteLine(GL.CheckFramebufferStatus(FramebufferTarget.Framebuffer).ToString());
 
             GL.BindFramebuffer(FramebufferTarget.Framebuffer, 0);
 
@@ -53,7 +55,7 @@ namespace Visualiser
         public void Start()
         {
             GL.BindFramebuffer(FramebufferTarget.Framebuffer, frameBufferID);
-            GL.Viewport(0, 0, width, height);
+            GL.Viewport(0, 0, width, height);   //half width, render, move, render
         }
 
         public void Stop()
@@ -198,14 +200,15 @@ namespace Visualiser
 
         public static void OpenTKWindow_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
         {
-            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            distortion.Start();
 
+            GL.ClearColor(System.Drawing.Color.Red);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
             GL.Disable(EnableCap.CullFace);
             GL.Enable(EnableCap.Blend);
             GL.BlendFunc(BlendingFactorSrc.SrcAlpha, BlendingFactorDest.OneMinusSrcAlpha);
             GL.Enable(EnableCap.DepthTest);
-
-            distortion.Start();
 
             shader.Bind();
             shader.SetUniform("ProjectionMatrix", ref camera.projMatrix);
@@ -229,11 +232,20 @@ namespace Visualiser
 
             distortion.Stop();
 
+            GL.ClearColor(System.Drawing.Color.Salmon);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            GL.Disable(EnableCap.CullFace);
+            GL.Enable(EnableCap.Blend);
+            GL.BlendFunc(BlendingFactorSrc.SrcAlpha, BlendingFactorDest.OneMinusSrcAlpha);
+            GL.Enable(EnableCap.DepthTest);
+
             int temp = GL.GenVertexArray();
             GL.BindVertexArray(temp);
             //GL.BindFramebuffer(FramebufferTarget.Framebuffer, distortion.frameBufferID);
             GL.ActiveTexture(TextureUnit.Texture0);
-            GL.BindTexture(TextureTarget.Texture2D, distortion.testTex);    //test texture works, framebuffer doesn't
+            GL.BindTexture(TextureTarget.Texture2D, distortion.textureID);    //test texture works, framebuffer doesn't
             distortion.distortionShader.Bind();
             distortion.distortionShader.SetUniform("Texture", 0);
             GL.DrawArrays(PrimitiveType.Points, 0, 1);
@@ -241,7 +253,7 @@ namespace Visualiser
             //GL.BindFramebuffer(FramebufferTarget.Framebuffer, 0);
             GL.BindVertexArray(0);
 
-            //GL.Disable(EnableCap.DepthTest);
+            //GL.Disable(EnableCap.DepthTest);*/
 
             //hud.Render();
 
